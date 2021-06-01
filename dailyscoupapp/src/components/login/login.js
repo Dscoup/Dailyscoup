1192f91af078924e2c65161bafbf5e7ac1342644
harish <harishkumar4518@gmail.com>
2021-06-01 07:56:24 +0530

    latest_source

--- dailyscoupapp/src/components/login/login.js
+++ dailyscoupapp/src/components/login/login.js
@@ -1,79 +1,192 @@
-import React, { PureComponent } from 'react'
-import { MDBContainer, MDBRow, MDBCol, MDBInput, MDBBtn } from 'mdbreact';
+import React ,{useState,useRef} from 'react';
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import Link from '@material-ui/core/Link';
+import Grid from '@material-ui/core/Grid';
+import Box from '@material-ui/core/Box';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+import './login.css';
+import { Facebook, Apple, Mail } from '@material-ui/icons';
 
-
-class Login extends PureComponent {
-    constructor(props) {
-        super(props)
-
-        this.state = {
-            
-        }
-    }
-
-    render() {
-        return (
-            <form>
-  {/* <!-- Email input --> */}
-  <div class="form-outline mb-4">
-    <input type="email" id="form2Example1" class="form-control" />
-    <label class="form-label" for="form2Example1">Email address</label>
-  </div>
-
-  {/* <!-- Password input --> */}
-  <div class="form-outline mb-4">
-    <input type="password" id="form2Example2" class="form-control" />
-    <label class="form-label" for="form2Example2">Password</label>
-  </div>
-
-  {/* <!-- 2 column grid layout for inline styling --> */}
-  <div class="row mb-4">
-    <div class="col d-flex justify-content-center">
-      {/* <!-- Checkbox --> */}
-      <div class="form-check">
-        <input
-          class="form-check-input"
-          type="checkbox"
-          value=""
-          id="form2Example3"
-          checked
-        />
-        <label class="form-check-label" for="form2Example3"> Remember me </label>
-      </div>
-    </div>
-
-    <div class="col">
-      {/* <!-- Simple link --> */}
-      <a href="#!">Forgot password?</a>
-    </div>
-  </div>
-
-  {/* <!-- Submit button --> */}
-  <button type="submit" class="btn btn-primary btn-block mb-4">Sign in</button>
-
-  {/* <!-- Register buttons --> */}
-  <div class="text-center">
-    <p>Not a member? <a href="#!">Register</a></p>
-    <p>or sign up with:</p>
-    <button type="button" class="btn btn-primary btn-floating mx-1">
-      <i class="fab fa-facebook-f"></i>
-    </button>
-
-    <button type="button" class="btn btn-primary btn-floating mx-1">
-      <i class="fab fa-google"></i>
-    </button>
-
-    <button type="button" class="btn btn-primary btn-floating mx-1">
-      <i class="fab fa-twitter"></i>
-    </button>
-
-    <button type="button" class="btn btn-primary btn-floating mx-1">
-      <i class="fab fa-github"></i>
-    </button>
-  </div>
-</form>
-        )
-    }
+function Copyright() {
+    return (
+        <Typography variant="body2" color="textSecondary" align="center">
+            {'Copyright © '}
+            <Link color="inherit" href="https://material-ui.com/">
+                Your Website
+      </Link>{' '}
+            {new Date().getFullYear()}
+            {'.'}
+        </Typography>
+    );
 }
 
-export default Login
+
+
+const useStyles = makeStyles((theme) => ({
+    paper: {
+        marginTop: theme.spacing(8),
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+    },
+    avatar: {
+        margin: theme.spacing(1),
+        backgroundColor: theme.palette.secondary.main,
+    },
+    form: {
+        width: '100%', // Fix IE 11 issue.
+        marginTop: theme.spacing(1),
+    },
+    submit: {
+        margin: theme.spacing(3, 0, 2),
+    },
+}));
+
+export default function Login() {
+    const classes = useStyles();
+    const [validation1, setValidation1] = useState(false);
+    const [validation2, setValidation2] = useState(false);
+    const [mailText, setEmailText] = useState('');
+    const [passText, setPassText] = useState('');
+    const valueRef1 = useRef(''); 
+    const valueRef2 = useRef(''); 
+
+
+    const handleSubmit= (e) => {
+        // alert('hit')
+        e.preventDefault();
+        if(valueRef1.current.value == '')
+        {
+            setValidation1(true);
+            setEmailText('Email should not be empty')
+        }
+        else
+        {
+            setValidation1(false);
+            setEmailText('')
+        }
+
+        if(valueRef2.current.value == '')
+        {
+            setValidation2(true);
+            setPassText('Password should not be empty')
+        }
+        else{
+            setValidation2(false);
+            setPassText('')
+        }
+        
+      }
+
+    return (
+        <div>
+            <h4>Welcome To</h4>
+            <h1>DAILY SCOUP</h1>
+            <Container component="main" maxWidth="xs">
+
+                <CssBaseline />
+                <div className={classes.paper}>
+                    <Avatar className={classes.avatar}>
+          <LockOutlinedIcon />
+        </Avatar>
+        
+         <h4>Are You a new user? join as now! </h4> 
+         <Typography component="h4" variant="h5"> 
+         <a href="#">Sign Up</a>
+         </Typography>
+                    <form className={classes.form} onSubmit ={e => { handleSubmit(e) }}>
+                        <TextField
+                            // variant="outlined"
+                            margin="normal"
+                            // required
+                            fullWidth
+                            id="email"
+                            label="Email Address"
+                            name="email"
+                            autoComplete="email"
+                            autoFocus
+                            helperText={mailText}
+                            error = {validation1}
+                            inputRef={valueRef1}
+                        />
+                        <TextField
+                            // variant="outlined"
+                            margin="normal"
+                            // required
+                            fullWidth
+                            name="password"
+                            label="Password"
+                            type="password"
+                            id="password"
+                            autoComplete="current-password"
+                            inputRef={valueRef2}
+                            error = {validation2}
+                            helperText={passText}
+                        />
+                        {/* <FormControlLabel
+            control={<Checkbox value="remember" color="primary" />}
+            label="Remember me"
+          /> */}
+                        <Button
+                            type="submit"
+                            fullWidth
+                            variant="contained"
+                            color="primary"
+                            className={classes.submit}
+                            
+                        >
+                            Sign In
+          </Button>
+                        <Grid container>
+                            <Grid item xs>
+                                <Link href="#" variant="body2">
+                                    Forgot password?
+              </Link>
+                            </Grid>
+                            <Grid item>
+                                <Link href="#" variant="body2">
+                                    {"Continue as guest? "}
+                                </Link>
+                            </Grid>
+
+
+                        </Grid>
+                        <br />
+                        <br />
+                        <p>
+                            Please Sign In with
+              </p>
+                        <Grid container>
+                            <Grid item xs>
+                                 <Avatar >
+                                    <Facebook />
+                                 </Avatar>
+                            </Grid>
+                            <Grid item xs>
+                                 <Avatar >
+                                    <Apple />
+                                 </Avatar>
+                            </Grid>
+                            <Grid item>
+                            <Avatar >
+                                    <Mail />
+                                 </Avatar>
+                            </Grid>
+                        </Grid>
+                        <br />
+                        <br />
+                    </form>
+                </div>
+
+            </Container>
+        </div>
+    );
+}
--- dailyscoupapp/src/components/register/register.js
+++ dailyscoupapp/src/components/register/register.js
@@ -1,74 +1,159 @@
-import React, { PureComponent } from 'react';
-import { withRouter } from "react-router-dom";
+import React from 'react';
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import Link from '@material-ui/core/Link';
+import Grid from '@material-ui/core/Grid';
+import Box from '@material-ui/core/Box';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+import './register.css';
+import { Facebook, Apple, Mail } from '@material-ui/icons';
 
-
-
-class Register extends PureComponent {
-  constructor(props) {
-    super(props);
-    this.state = {
-      email: '',
-      password: ''
-    };
-
-    // This binding is necessary to make `this` work in the callback
-
-    this.renderLogin = this.renderLogin.bind(this);
-    this.renderAuth = this.renderAuth.bind(this);
-
-  }
-
-  renderLogin() {
-    // window.location = '/';
-    this.props.history.push("/");
-    // this.props.history.push("/register");
-    // history.push('/register');
-    // this.context.router.history.push('/register');
-  }
-  renderAuth() {
-    window.location = '/authentication';
-  }
-  render() {
+function Copyright() {
     return (
-      <div>
-        <div id="iconGroup">
-          <Facebook />
-
-        </div>
-        <div id="loginform">
-
-          <div>
-
-            <FormInput placeholder="First Name" type="text" />
-            <FormInput placeholder="Last Name" type="text" />
-            <FormInput placeholder="Email" type="email" />
-            <FormInput placeholder="Password" type="password" />
-            {/* <FormButton onClick={this.renderAuth} title="SIGN UP" /> */}
-            <div id="button" class="row">
-              <button onClick={this.renderAuth}>SIGN UP</button>
-            </div>
-          </div>
-          <div id="alternativeLogin">
-            <label onClick={this.renderLogin}>Already Have a account? Sign In</label>
-
-          </div>
-        </div>
-      </div>
-    )
-  }
+        <Typography variant="body2" color="textSecondary" align="center">
+            {'Copyright © '}
+            <Link color="inherit" href="https://material-ui.com/">
+                Your Website
+      </Link>{' '}
+            {new Date().getFullYear()}
+            {'.'}
+        </Typography>
+    );
 }
-export default withRouter(Register)
+
+const useStyles = makeStyles((theme) => ({
+    paper: {
+        marginTop: theme.spacing(8),
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+    },
+    avatar: {
+        margin: theme.spacing(1),
+        backgroundColor: theme.palette.secondary.main,
+    },
+    form: {
+        width: '100%', // Fix IE 11 issue.
+        marginTop: theme.spacing(1),
+    },
+    submit: {
+        margin: theme.spacing(3, 0, 2),
+    },
+}));
+
+export default function SignUp() {
+    const classes = useStyles();
+
+    return (
+        <div>
+            <h1>DAILY SCOUP</h1>
+            <Container component="main" maxWidth="xs">
+
+                <CssBaseline />
+                <div className={classes.paper}>
+                    {/* <Avatar className={classes.avatar}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          Login
+        </Typography> */}
+                    <form className={classes.form} noValidate>
+                    <TextField
+                            // variant="outlined"
+                            margin="normal"
+                            required
+                            fullWidth
+                            id="username"
+                            label="User Name"
+                            name="username"
+                            autoComplete="name"
+                            autoFocus
+                        />
+                        <TextField
+                            // variant="outlined"
+                            margin="normal"
+                            required
+                            fullWidth
+                            id="email"
+                            label="Email Address"
+                            name="email"
+                            autoComplete="email"
+                            autoFocus
+                        />
+                        <TextField
+                            // variant="outlined"
+                            margin="normal"
+                            required
+                            fullWidth
+                            name="password"
+                            label="Password"
+                            type="password"
+                            id="password"
+                            autoComplete="current-password"
+                        />
+                        {/* <FormControlLabel
+            control={<Checkbox value="remember" color="primary" />}
+            label="Remember me"
+          /> */}
+                        <p>By clicking and option below, I agree to the Terms of use and have read the privarcy statement</p>
+                        <Button
+                            type="submit"
+                            fullWidth
+                            variant="contained"
+                            color="primary"
+                            className={classes.submit}
+                        >
+                            Sign In
+          </Button>
+                        <Grid container>
+                            <Grid item xs>
+                                <Link href="#" variant="body2">
+                                    Forgot password?
+              </Link>
+                            </Grid>
+                            <Grid item>
+                                <Link href="#" variant="body2">
+                                    {"Continue as guest? "}
+                                </Link>
+                            </Grid>
 
 
-const FormInput = props => (
-  <div class="row">
-    <label>{props.description}</label>
-    <input type={props.type} placeholder={props.placeholder} />
-  </div>
-);
-const Facebook = props => (
-  <a href="#" id="facebookIcon" />
-);
+                        </Grid>
+                        <br />
+                        <br />
+                        <p>
+                            Please Sign Up with
+              </p>
+                        <Grid container>
+                            <Grid item xs>
+                                 <Avatar >
+                                    <Facebook />
+                                 </Avatar>
+                            </Grid>
+                            <Grid item xs>
+                                 <Avatar >
+                                    <Apple />
+                                 </Avatar>
+                            </Grid>
+                            <Grid item>
+                            <Avatar >
+                                    <Mail />
+                                 </Avatar>
+                            </Grid>
+                        </Grid>
+                        <br />
+                        <br />
+                    </form>
+                </div>
 
-
-
+            </Container>
+        </div>
+    );
+}
